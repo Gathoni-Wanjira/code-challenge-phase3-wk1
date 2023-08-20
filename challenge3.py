e8769b06f8af8f04d2bfd19e675fe6b3d97e6c32
Gathoni <janetgathoni910@gmail.com>
2023-08-20 07:29:41 +0300

    Done

--- challenge3.py
+++ challenge3.py
@@ -1,21 +1,17 @@
+
+def get_consonant_value(char):
+    return ord(char) - ord('a') + 1
+
 def highest_consonant_value(s):
     consonant_values = []
     current_value = 0
-
-    consonant_mapping = {
-        'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5,
-        'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
-        'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15,
-        'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20,
-        'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26
-    }
 
     for char in s:
         if char in "aeiou":
             consonant_values.append(current_value)
             current_value = 0
         else:
-            current_value += consonant_mapping[char]
+            current_value += get_consonant_value(char)
 
     consonant_values.append(current_value)
     return max(consonant_values)
